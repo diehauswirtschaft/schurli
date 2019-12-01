f8a2a03ff58670ccc39cf90f9265b6a120977518
Philipp Naderer-Puiu <philipp@naderer-puiu.at>
2019-12-01 12:03:56 +0100

    Adds ignore cookie

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const matomo = require("./controllers/matomo");
 const firestore = require("./controllers/firestore");
 
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const { param, validationResult } = require("express-validator");
 
 const app = express();
@@ -10,6 +11,9 @@ const app = express();
 // configure express to be less noisy
 app.set("etag", false); // we don't need etags ...
 app.set("x-powered-by", false); // nobody needs this header
+
+// for the ignore cookie
+app.use(cookieParser());
 
 // security headers
 app.use((req, res, next) => {
@@ -41,16 +45,19 @@ function getQuery(originalURL) {
  * A handler for the base URLs and the /s URL. Redirects to the configured base URL.
  */
 app.get(["/", "/s/"], (req, res) => {
+    const ignore = req.cookies && req.cookies.ignore;
     const fullURL = config.get("baseURL") + "/";
 
     try {
-        matomo.trackVisit(
-            "",
-            fullURL,
-            req.headers["user-agent"] || "",
-            req.headers["referer"] || "",
-            getQuery(req.originalUrl)
-        );
+        if (!ignore) {
+            matomo.trackVisit(
+                "",
+                fullURL,
+                req.headers["user-agent"] || "",
+                req.headers["referer"] || "",
+                getQuery(req.originalUrl)
+            );
+        }
 
         return res.redirect(302, fullURL);
     } catch (e) {
@@ -65,6 +72,7 @@ app.get(["/", "/s/"], (req, res) => {
 app.get("/s/:slug", [
     param("slug").isByteLength({ min: 1, max: 200 })
 ], async (req, res) => {
+    const ignore = req.cookies && req.cookies.ignore;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).send("Invalid short URL!");
@@ -78,13 +86,15 @@ app.get("/s/:slug", [
         if (fullURL === null) {
             return res.status(404).send("Not found.");
         } else {
-            matomo.trackVisit(
-                slug,
-                fullURL,
-                req.headers["user-agent"] || "",
-                req.headers["referer"] || "",
-                getQuery(req.originalUrl)
-            );
+            if (!ignore) {
+                matomo.trackVisit(
+                    slug,
+                    fullURL,
+                    req.headers["user-agent"] || "",
+                    req.headers["referer"] || "",
+                    getQuery(req.originalUrl)
+                );
+            }
 
             return res.redirect(302, fullURL);
         }
@@ -92,6 +102,33 @@ app.get("/s/:slug", [
         console.error(e);
         return res.status(500).send("Internal Server Error.");
     }
+});
+
+/**
+ * Sets a cookie which will ignore incoming requests for tracking,
+ * but will redirect them as well.
+ */
+app.get(["/ignore"], (req, res) => {
+    res.cookie("ignore", "true", {
+        domain: config.get("cookie:domain"),
+        maxAge: config.get("cookie:maxAge"),
+        secure: false
+    });
+
+    res.status(200).send("\uD83C\uDF6A Cookie set \u2705");
+});
+
+/**
+ * Deletes the ignore cookie.
+ */
+app.get(["/count"], (req, res) => {
+    res.clearCookie("ignore", {
+        domain: config.get("cookie:domain"),
+        maxAge: config.get("cookie:maxAge"),
+        secure: false
+    });
+
+    res.status(200).send("\uD83C\uDF6A Cookie deleted \u274C");
 });
 
 module.exports = app;
